9b3d8d46f7435611324102b1aee9584f28ce34f0
Sajid Shaikh <shaikhsajid11112000@gmail.com>
2026-09-08 17:36:25 +0530

    Bumped version

--- src/b2b_firmographic_crawler/__init__.py
+++ src/b2b_firmographic_crawler/__init__.py
@@ -18,7 +18,7 @@ from b2b_firmographic_crawler.sources.base import SourceProvider
 from b2b_firmographic_crawler.sources.craft_source import CraftSource
 from b2b_firmographic_crawler.sources.registry import SourceRegistry
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 __all__ = [
     "B2BFirmographicCrawler",
